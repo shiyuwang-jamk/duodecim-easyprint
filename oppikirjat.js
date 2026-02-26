1f1898ed59041270ed67f656a351181963f0f136
Wang Dani <daniprogger@outlook.com>
2026-02-26 10:53:50 +0200

    Unified style with tportti.js

--- oppikirjat.js
+++ oppikirjat.js
@@ -10,32 +10,81 @@
 
 
 // via https://superuser.com/questions/1337469/grasemonkey-change-design-css
-function GM_addStyle(cssStr){
-
+function GM_addPrintStyle(cssStr) {
     var n = document.createElement('style');
     n.type = "text/css";
-    n.innerHTML = cssStr;
+    n.innerHTML = "@media print {" + cssStr + "}";
     document.getElementsByTagName('head')[0].appendChild(n);
-
 }
 
-GM_addStyle("@media print {article section[role=main] {column-count: 1;}}");
-GM_addStyle("@media print {article {column-count: 2!important;}}");
+// Put header in same column as whole article
+GM_addPrintStyle("article section[role=main] {column-count: 1;}");
+// Split header and article
+GM_addPrintStyle("article {column-count: 2!important;}");
 
 // TODO: include header and footer
-// GM_addStyle("@media print {article .onecol {column-count: 1!important;}}");
-// GM_addStyle("@media print {article .d-document{column-count: 2!important;}}");
+// GM_addPrintStyle("article .onecol {column-count: 1!important;}}");
+// GM_addPrintStyle("article .d-document{column-count: 2!important;}}");
 
-// GM_addStyle("@media print {article img {page-break-inside: avoid!important;max-height: 95vh!important;max-width: 100%!important;width: 100%!important;}}");
-GM_addStyle("@media print {article img {width: 95%!important}}");
-GM_addStyle("@media print {div .d-print-block {display: none!important}}");
+// GM_addPrintStyle("article img {page-break-inside: avoid!important;max-height: 95vh!important;max-width: 100%!important;width: 100%!important;}}");
+GM_addPrintStyle("img {min-width: 95%; max-width: 100%!important}");
+GM_addPrintStyle("div .d-print-block {display: none!important}");
 
 // TODO new page before table/img
-GM_addStyle("@media print {caption var {page-break-before: always!important}}");
+GM_addPrintStyle("caption var {page-break-before: always!important}");
 
 
 // no scroll bar
-GM_addStyle("@media print {* {overflow: hidden!important;}}");
+GM_addPrintStyle("* {overflow: hidden!important;}");
 
-GM_addStyle("@media print { @page {size: landscape!important}}");
-// GM_addStyle("@media print {.d-article header .d-database, .d-updated .d-authors {display: none!important}}");
+GM_addPrintStyle("@page {size: a4!important}");
+// No top badge/banner
+GM_addPrintStyle(".d-none {display: none!important}}");
+// GM_addPrintStyle("GM_addPrintStyle.d-article header .d-database, .d-updated .d-authors {display: none!important}}");
+
+//  change title to h1 to match context and avoid clicking back and forth
+// via https://stackoverflow.com/questions/76791476/how-to-change-the-title-with-userscripts
+function fixTitleOP() {
+    var title = document.title;
+
+        title = document.querySelector('h1').outerText +
+            ' | ' + document.querySelector('div.d-database').outerText // Name of book
+            + ' - Dodecim Oppiportti'; // Database name
+    
+    return title;
+}
+
+// works on selecting from e.g. right-slide navigation tree panel, and in-text buttons
+// When browsing textbooks in Oppiportti, all clicks, including redirecting to #ids of the same page, triggers loading.
+// https://stackoverflow.com/questions/20613754/javascript-eventlistener-for-update-change-on-element-innertext
+window.addEventListener("load", () => {
+    document.title = fixTitleOP() // userscript: works on visiting a link in a new tab or refreshing the page
+    
+    // works on browser forward/backward switching betwen TP pages
+    // https://stackoverflow.com/questions/3522090/event-when-window-location-href-changes
+    let oldHref = document.location.href;
+    // const body = document.querySelector('body');
+    const loadObserver = new MutationObserver((mutations) => {
+        // mutations.forEach((mutation) => {
+        //     onloadIteration++;
+        // });
+        if (oldHref !== document.location.href) {
+            oldHref = document.location.href;
+            // urlChange++;
+            document.title = fixTitleOP();
+        }
+    });
+    loadObserver.observe(document.querySelector('body'), {
+        childList: true,
+        subtree: true
+    });
+    // window.alert('Upon (re)loading, iteration = ' + onloadIteration
+    // + '\nURL changed caugut = ' + urlChange
+    // + '\ndocument.title = ' + document.title);
+    // loadObserver.disconnect();
+});
+var config = {
+    characterData: true,
+    subtree: true,
+    childList: true
+}; // Lääketietokanta: In article mode, mutations are several levels below app-article, which subtree won't observe recursively LTK#4
